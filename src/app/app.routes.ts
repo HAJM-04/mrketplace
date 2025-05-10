fbcba0b7a9590020108fc1a85cfc7541710a1471
Jackson Grim <jackson.grim@redboyconsulting.com>
2025-05-10 11:01:29 -0600

    Applying router Global

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { 
+        path: '', redirectTo: 'auth/login', pathMatch: 'full' 
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    },
+    {
+        path: 'products',
+        loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+    },
+    {
+        path: 'sales',
+        loadChildren: () => import('./sales/sales.module').then(m => m.SalesModule)
+    },
+];
