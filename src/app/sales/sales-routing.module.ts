e7993f42ec58645875181d63d48da9b0eeb5644c
Jackson Grim <jackson.grim@redboyconsulting.com>
2025-05-11 13:53:24 -0600

    Adding routes to all modules

--- src/app/sales/sales-routing.module.ts
+++ src/app/sales/sales-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { SalesHistoryComponent } from './sales-history/sales-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart', component: CartComponent },
+  { path: 'history', component: SalesHistoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
