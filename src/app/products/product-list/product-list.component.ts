4e419a8a8a7c97287985e95bc3c4f20ef4df8c9e
Jackson Grim <jackson.grim@redboyconsulting.com>
2025-05-16 18:28:24 -0600

    Hardcoding the products view

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-
+  products = [
+    {
+      name: 'COVENANT SLEEVE TEE',
+      description: '“Covenant” is track #5 of the album “Of Demons & Mortals.”',
+      price: 50,
+      image: 'assets/images/clothes/covenant-sleeve-tee-front.png',
+    },
+    {
+      name: 'COVENANT COACH JACKET',
+      description: '“Covenant” is track #5 of the album “Of Demons & Mortals.”',
+      price: 100,
+      image: 'assets/images/clothes/covenant-coach-jacket-front.png',
+    },
+    {
+      name: 'COVENANT TEE SHIRT',
+      description: '“Covenant” is track #5 of the album “Of Demons & Mortals.”',
+      price: 45,
+      image: 'assets/images/clothes/covenant-tee-shirt-front.png',
+    },
+  ];
 }
