ace5cdba1381c52bdf1c83a1032e1d58328af6f5
Jackson Grim <jackson.grim@redboyconsulting.com>
2025-05-16 18:24:50 -0600

    Setting UP the look n feel of Login window

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
+  standalone: true,
   selector: 'app-login',
-  imports: [],
+  imports: [ FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
 
+  constructor(private router: Router) {}
+
+  login() {
+    // Validación básica (solo para demostrar)
+    if (this.email && this.password) {
+      localStorage.setItem('user', JSON.stringify({ email: this.email }));
+      this.router.navigate(['/products']);
+    } else {
+      this.errorMessage = 'Por favor, ingresa tu correo y contraseña.';
+    }
+  }
 }
