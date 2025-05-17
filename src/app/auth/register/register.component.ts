112c03e02b17d35dbb83790f6b91bcfb97a3a068
Jackson Grim <jackson.grim@redboyconsulting.com>
2025-05-16 18:25:20 -0600

    Setting UP the look n feel of Signup  window

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  name = '';
+  surname = '';
+  email = '';
+  password = '';
+  passwordConfirmation = '';
 
+  register() {
+    if (this.password !== this.passwordConfirmation) {
+      alert('Passwords do not match!');
+      return;
+    }
+
+    // Aquí puedes guardar en localStorage o hacer una petición a backend
+    console.log('Registering:', {
+      name: this.name,
+      surname: this.surname,
+      email: this.email,
+      password: this.password,
+    });
+
+    alert('Successfully registered!');
+  }
 }
