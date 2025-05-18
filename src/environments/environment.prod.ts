4da92cd50a06c72826bb2d23f99d2aa4b7037896
Jackson Grim <jackson.grim@redboyconsulting.com>
2025-05-18 16:29:27 -0600

    Adding production file and pipeline changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAiGP5BPOz1hE9RSqEcg_isn1MjbOuo2d4',
+    authDomain: 'marketplace-a9ab1.firebaseapp.com',
+    projectId: 'marketplace-a9ab1',
+    storageBucket: 'marketplace-a9ab1.firebasestorage.app',
+    messagingSenderId: '852699536174',
+    appId: '1:852699536174:web:1ba257ec00e641d62df8bf',
+    measurementId: 'G-Y07CXG90G3'
+  }
+};
