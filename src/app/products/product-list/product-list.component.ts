bed5da40c94211d98c8baf0ee7ecdf6daf208ff0
Jackson Grim <1688498+jacksongrimmx@users.noreply.github.com>
2025-06-09 21:31:31 -0600

    Giving functionallities to cart merge

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProductService, Product } from '../product-services';
+import { CartService } from '../../sales/cart.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
@@ -12,13 +15,38 @@ import { ProductService, Product } from '../product-services';
 })
 export class ProductListComponent implements OnInit  {
   products: Product[] = [];
+  selectedSizes: { [productId: string]: string } = {}; // Size Mapping
+  cartCount$!: Observable<number>;    
 
-  constructor(private productService: ProductService) {}
+  // Toast Message and Flag
+  toastMessage = '';
+  showToast = false;
+
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
     });
+    this.cartCount$ = this.cartService
+    .getCartItems()
+    .pipe(map(items => items.reduce((acc, i) => acc + i.units, 0)));
+  }
+
+  onAddToCart(product: Product, size: string): void {
+    this.cartService.addToCart(product, size);
+    // opcional: mostrar un toast o console.log
+    this.toast(`✓ ${product.title} (talla ${size}) añadido al carrito`);
+    console.log(`Añadido al carrito:`, product.title, `talla`, size);
+  }
+
+  // Toast Notification Method
+  private toast(msg: string) {
+    this.toastMessage = msg;
+    this.showToast = true;
+    setTimeout(() => this.showToast = false, 3000);
   }
 }
-
